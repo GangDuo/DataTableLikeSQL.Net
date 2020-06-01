ad320f9d7ef7e3d3c3bbbc0f4e36f46f762a10a1
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2020-06-01 10:59:38 +0900

    setup project

---
+++ DDL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace DataTableLikeSQL
+{
+    public class DDL
+    {
+        public static DataTable CreateTable(string tableName, DataColumn[] columns)
+        {
+            var table = new DataTable(tableName);
+            table.Columns.AddRange(columns);
+            return table;
+        }
+
+    }
+}
---
+++ Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace DataTableLikeSQL
+{
+    public static class Extensions
+    {
+        public static void AlterTableAddPrimaryKey(this DataTable table, string[] columnName)
+        {
+            var keys = new List<DataColumn>();
+            foreach (DataColumn clm in table.Columns)
+            {
+                if (columnName.Contains(clm.ColumnName))
+                {
+                    keys.Add(clm);
+                }
+            }
+            Debug.Assert(keys.Count > 0);
+            table.PrimaryKey = keys.ToArray();
+        }
+
+    }
+}
